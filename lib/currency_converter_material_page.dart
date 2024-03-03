0aace935e44280449577ea19c54917d480b20fc3
thenaijadev <thenaijadev@gmailcom>
2024-03-03 11:40:08 +0100

    cloned changes

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterHomePage extends StatelessWidget {
@@ -7,41 +8,65 @@ class CurrencyConverterHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     const border = OutlineInputBorder(
       borderSide: BorderSide(
-          color: Colors.green,
-          width: 2.0,
+          color: Colors.white,
+          width: 1.0,
           style: BorderStyle.solid,
           strokeAlign: BorderSide.strokeAlignInside),
       borderRadius: BorderRadius.all(
         Radius.circular(5),
       ),
     );
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
                   fontSize: 45,
                   fontWeight: FontWeight.bold,
                   color: Color.fromRGBO(255, 255, 255, 1)),
             ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'please enter the amount in USD',
-                hintStyle: TextStyle(
-                  color: Colors.black,
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'please enter the amount in USD',
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
                 ),
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                prefixIconColor: Colors.black,
-                filled: true,
-                fillColor: Colors.white,
-                focusedBorder: border,
-                enabledBorder: border,
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print('button clicked');
+                  }
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.black,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                child: const Text(
+                  'Convert',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            ),
           ],
         ),
       ),
